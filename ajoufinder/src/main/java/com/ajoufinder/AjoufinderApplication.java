b3186d19d09a7faeaad2fe21768e8ace4c26b010
JeongIna <jeongina89@gmail.com>
2024-10-31 23:31:16 +0900

    프로젝트 초기설정

---
+++ ajoufinder/src/main/java/com/ajoufinder/AjoufinderApplication.java
@@ -0,0 +1,13 @@
+package com.ajoufinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjoufinderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AjoufinderApplication.class, args);
+  }
+
+}
---
+++ ajoufinder/src/test/java/com/ajoufinder/AjoufinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajoufinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjoufinderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
