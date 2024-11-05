7db8d7f9e8df545aa385a997c9419a3554bf8d69
JeongIna <jeongina89@gmail.com>
2024-11-05 19:26:06 +0900

    feat: UserController의 회원가입 api 개발 (/api/v1/users/register)

---
+++ ajoufinder/src/main/java/com/ajoufinder/api/controller/user/UserController.java
@@ -0,0 +1,25 @@
+package com.ajoufinder.api.controller.user;
+
+import com.ajoufinder.api.controller.user.dto.request.UserCreateRequestDto;
+import com.ajoufinder.api.service.user.UserCommandService;
+import com.ajoufinder.api.service.user.UserQueryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController("/api/v1/users")
+public class UserController {
+  private final UserCommandService userCommandService;
+  private final UserQueryService userQueryService;
+
+  @PostMapping("/register")
+  public ResponseEntity<Void> register(@RequestBody @Valid UserCreateRequestDto dto) {
+    userCommandService.createUser(dto);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+}
