51c88dfd9fe15a8692fc359acea5ddc4b7a82b81
JeongIna <jeongina89@gmail.com>
2024-11-07 16:41:27 +0900

    feat: BoardController에 '잃었어요'&'주웠어요' 게시글 생성 api/삭제 api 구현

--- ajoufinder/src/main/java/com/ajoufinder/api/controller/board/BoardController.java
+++ ajoufinder/src/main/java/com/ajoufinder/api/controller/board/BoardController.java
@@ -1,4 +1,40 @@
 package com.ajoufinder.api.controller.board;
 
+import com.ajoufinder.api.controller.board.dto.request.BoardCreateRequestDto;
+import com.ajoufinder.api.controller.board.dto.response.BoardCreateResponseDto;
+import com.ajoufinder.api.service.board.BoardCommandService;
+import com.ajoufinder.api.service.board.BoardQueryService;
+import com.ajoufinder.domain.board.entity.Board;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/boards")
+@RequiredArgsConstructor
 public class BoardController {
+  private final BoardQueryService boardQueryService;
+  private final BoardCommandService boardCommandService;
+
+  @PostMapping("/lost")
+  public ResponseEntity<BoardCreateResponseDto> createLostBoard(@RequestBody @Valid BoardCreateRequestDto requestDto) {
+    Board board=boardCommandService.createLostBoard(requestDto);
+    BoardCreateResponseDto responseDto = BoardCreateResponseDto.from(board);
+    return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+  }
+
+  @PostMapping("/found")
+  public ResponseEntity<BoardCreateResponseDto> createFoundBoard(@RequestBody @Valid BoardCreateRequestDto requestDto) {
+    Board board=boardCommandService.createFoundBoard(requestDto);
+    BoardCreateResponseDto responseDto = BoardCreateResponseDto.from(board);
+    return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/{boardId}")
+  public ResponseEntity<Void>deleteBoard(@PathVariable Long boardId){
+    boardCommandService.deleteBoard(boardId);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
 }
