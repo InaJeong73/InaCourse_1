51ef621d1e7403ee8754532a4daae4a2b9e3ae67
JeongIna <jeongina89@gmail.com>
2024-11-07 19:25:55 +0900

    feat: LocationCommandService에 Location 삭제&생성 로직 구현

--- ajoufinder/src/main/java/com/ajoufinder/api/service/location/LocationCommandService.java
+++ ajoufinder/src/main/java/com/ajoufinder/api/service/location/LocationCommandService.java
@@ -1,10 +1,34 @@
 package com.ajoufinder.api.service.location;
 
+import com.ajoufinder.api.controller.location.dto.request.LocationCreateRequestDto;
+import com.ajoufinder.common.exception.ExceptionCode;
+import com.ajoufinder.common.exception.location.LocationException;
+import com.ajoufinder.domain.location.entity.Location;
+import com.ajoufinder.domain.location.entity.constant.LocationStatus;
+import com.ajoufinder.domain.location.repository.LocationRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class LocationCommandService {
+  private final LocationRepository locationRepository;
+  private final LocationQueryService locationQueryService;
+
+  public void createLocation(LocationCreateRequestDto requestDto) {
+    String locationName=requestDto.locationName();
+    if(locationRepository.existsByLocationName(locationName)){
+      throw new LocationException(ExceptionCode.DUPLICATED_LOCATION);
+    }
+    locationRepository.save(requestDto.toEntity());
+  }
+
+  public void deleteLocation(Long locationId) {
+    Location location=locationQueryService.findLocationByIdOrThrow(locationId);
+    location.changeStatus(LocationStatus.DELETED);
+    locationRepository.save(location);
+  }
 }
