6fa01d322678c6327fd6ba235c1b461074be6338
JeongIna <jeongina89@gmail.com>
2024-11-07 19:25:02 +0900

    feat: Location 에 상태 저장하는 status 필드 추가

--- ajoufinder/src/main/java/com/ajoufinder/domain/location/entity/Location.java
+++ ajoufinder/src/main/java/com/ajoufinder/domain/location/entity/Location.java
@@ -2,15 +2,19 @@ package com.ajoufinder.domain.location.entity;
 
 import com.ajoufinder.domain.TimeStamp;
 import com.ajoufinder.domain.board.entity.Board;
+import com.ajoufinder.domain.location.entity.constant.LocationStatus;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Location extends TimeStamp {
   @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -20,8 +24,20 @@ public class Location extends TimeStamp {
   @Column(name="location_name", length = 100)
   private String locationName;
 
+  @Enumerated(EnumType.STRING)
+  private LocationStatus status;
+
   @OneToMany(mappedBy="location",fetch = FetchType.LAZY)
   @JsonManagedReference
   private List<Board>boards=new ArrayList<>();
 
+  @Builder
+  public Location(String locationName) {
+    this.locationName = locationName;
+    this.status=LocationStatus.ACTIVE;
+  }
+
+  public void changeStatus(LocationStatus newStatus) {
+    this.status=newStatus;
+  }
 }
