a679a8d842a32cf0f748089e5559abcd4d3086a3
JeongIna <jeongina89@gmail.com>
2024-11-07 19:26:53 +0900

    feat: LocationCreateRequestDto 구현

---
+++ ajoufinder/src/main/java/com/ajoufinder/api/controller/location/dto/request/LocationCreateRequestDto.java
@@ -0,0 +1,16 @@
+package com.ajoufinder.api.controller.location.dto.request;
+
+import com.ajoufinder.domain.location.entity.Location;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record LocationCreateRequestDto(
+        @NotBlank(message="위치/건물명은 필수 입력값입니다.")
+        String locationName
+){
+  public Location toEntity() {
+    return Location.builder().locationName(locationName).build();
+  }
+}
