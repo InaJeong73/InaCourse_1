796637c7a8b5bc8faaa26e65194fa7b1f2015198
JeongIna <jeongina89@gmail.com>
2024-11-08 20:04:58 +0900

    feat: BoardRepository에 부모 인터페이스 추가

--- ajoufinder/src/main/java/com/ajoufinder/domain/board/repository/BoardRepository.java
+++ ajoufinder/src/main/java/com/ajoufinder/domain/board/repository/BoardRepository.java
@@ -1,9 +1,10 @@
 package com.ajoufinder.domain.board.repository;
 
 import com.ajoufinder.domain.board.entity.Board;
+import com.ajoufinder.domain.board.repository.custom.BoardRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Board,Long> {
+public interface BoardRepository extends JpaRepository<Board,Long>, BoardRepositoryCustom {
 }
