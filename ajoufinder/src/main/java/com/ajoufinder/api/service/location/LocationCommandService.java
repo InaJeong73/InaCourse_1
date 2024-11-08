7c6c0364bf8e6dedf1cf2de3ce28771c82625fcf
JeongIna <jeongina89@gmail.com>
2024-11-08 18:07:06 +0900

    feat: 위치명 중복체크하는 메소드 checkDuplicateLocationName()로 분리하고 위치 수정&위치 상태 변경 로직 개발

--- ajoufinder/src/main/java/com/ajoufinder/api/service/location/LocationCommandService.java
+++ ajoufinder/src/main/java/com/ajoufinder/api/service/location/LocationCommandService.java
@@ -1,6 +1,6 @@
 package com.ajoufinder.api.service.location;
 
-import com.ajoufinder.api.controller.location.dto.request.LocationCreateRequestDto;
+import com.ajoufinder.api.controller.location.dto.request.LocationRequestDto;
 import com.ajoufinder.common.exception.ExceptionCode;
 import com.ajoufinder.common.exception.location.LocationException;
 import com.ajoufinder.domain.location.entity.Location;
@@ -8,7 +8,6 @@ import com.ajoufinder.domain.location.entity.constant.LocationStatus;
 import com.ajoufinder.domain.location.repository.LocationRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -18,17 +17,27 @@ public class LocationCommandService {
   private final LocationRepository locationRepository;
   private final LocationQueryService locationQueryService;
 
-  public void createLocation(LocationCreateRequestDto requestDto) {
-    String locationName=requestDto.locationName();
-    if(locationRepository.existsByLocationName(locationName)){
-      throw new LocationException(ExceptionCode.DUPLICATED_LOCATION);
-    }
+  public void createLocation(LocationRequestDto requestDto) {
+    checkDuplicateLocationName(requestDto.locationName());
     locationRepository.save(requestDto.toEntity());
   }
 
-  public void deleteLocation(Long locationId) {
+  public void updateStatus(Long locationId, LocationStatus status) {
     Location location=locationQueryService.findLocationByIdOrThrow(locationId);
-    location.changeStatus(LocationStatus.DELETED);
+    location.changeStatus(status);
     locationRepository.save(location);
+  }
+
+  public void updateLocation(LocationRequestDto requestDto, Long locationId) {
+    checkDuplicateLocationName(requestDto.locationName());
+    Location location=locationQueryService.findLocationByIdOrThrow(locationId);
+    location.updateFromDto(requestDto);
+    locationRepository.save(location);
+  }
+
+  public void checkDuplicateLocationName(String locationName) {
+    if(locationRepository.existsByLocationNameAndStatus(locationName, LocationStatus.ACTIVE)){
+      throw new LocationException(ExceptionCode.DUPLICATED_LOCATION);
+    }
   }
 }
