3e659753c3e35bb2698920aa4d988c6a53919976
JeongIna <jeongina89@gmail.com>
2024-11-08 18:28:02 +0900

    feat: LocationController에 @GetMapping() api 구현

--- ajoufinder/src/main/java/com/ajoufinder/api/controller/location/LocationController.java
+++ ajoufinder/src/main/java/com/ajoufinder/api/controller/location/LocationController.java
@@ -1,6 +1,7 @@
 package com.ajoufinder.api.controller.location;
 
 import com.ajoufinder.api.controller.location.dto.request.LocationRequestDto;
+import com.ajoufinder.api.controller.location.dto.response.LocationResponseDto;
 import com.ajoufinder.api.service.location.LocationCommandService;
 import com.ajoufinder.api.service.location.LocationQueryService;
 import com.ajoufinder.domain.location.entity.constant.LocationStatus;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/locations")
@@ -21,6 +24,11 @@ public class LocationController {
   public ResponseEntity<String> createLocation(@RequestBody @Valid LocationRequestDto requestDto) {
     locationCommandService.createLocation(requestDto);
     return new ResponseEntity<>("위치가 추가되었습니다.",HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<LocationResponseDto>> getAllLocations() {
+    return new ResponseEntity<>(locationQueryService.findAllLocations(), HttpStatus.OK);
   }
 
   @DeleteMapping("/{locationId}")
