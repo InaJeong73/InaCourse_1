72e7f838e17d5e39dd030ce943cf9dfe26500f77
JeongIna <jeongina89@gmail.com>
2024-11-08 18:08:54 +0900

    feat: 위치id와 위치 상태를 가진 레코드의 존재 여부를 확인하는 메소드로 변경 existsByLocationNameAndStatus

--- ajoufinder/src/main/java/com/ajoufinder/domain/location/repository/LocationRepository.java
+++ ajoufinder/src/main/java/com/ajoufinder/domain/location/repository/LocationRepository.java
@@ -1,10 +1,11 @@
 package com.ajoufinder.domain.location.repository;
 
 import com.ajoufinder.domain.location.entity.Location;
+import com.ajoufinder.domain.location.entity.constant.LocationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
-  boolean existsByLocationName(String locationName);
+  boolean existsByLocationNameAndStatus(String locationName, LocationStatus status);
 }
