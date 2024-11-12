400a59aad2b1cdb6038c780ae9536a891fc92707
JeongIna <jeongina89@gmail.com>
2024-11-12 15:57:42 +0900

    feat: 데이터베이스에서 필요한 정보만 가져와 담는 BoardTempDto 생성

---
+++ ajoufinder/src/main/java/com/ajoufinder/api/controller/board/dto/response/BoardTempDto.java
@@ -0,0 +1,20 @@
+package com.ajoufinder.api.controller.board.dto.response;
+
+import com.ajoufinder.domain.board.entity.constant.BoardStatus;
+import com.ajoufinder.domain.board.entity.constant.ItemType;
+
+import java.time.LocalDateTime;
+
+public record BoardTempDto(
+        Long boardId,
+        Long userId,
+        String nickname,
+        Long locationId,
+        String locationName,
+        String title,
+        LocalDateTime relatedDate,
+        ItemType itemType,
+        BoardStatus boardStatus
+) {
+
+}
