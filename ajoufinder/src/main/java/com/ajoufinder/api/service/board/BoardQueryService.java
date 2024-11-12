62d6dd8527906474358007b8a8fa941d08b44c79
JeongIna <jeongina89@gmail.com>
2024-11-12 16:02:56 +0900

    feat: 잃었어요' 게시글 리스트 필터링 조회 비즈니스 로직 구현

--- ajoufinder/src/main/java/com/ajoufinder/api/service/board/BoardQueryService.java
+++ ajoufinder/src/main/java/com/ajoufinder/api/service/board/BoardQueryService.java
@@ -2,17 +2,25 @@ package com.ajoufinder.api.service.board;
 
 import com.ajoufinder.api.controller.board.dto.response.BoardDetailInfoResponseDto;
 import com.ajoufinder.api.controller.board.dto.response.BoardSimpleInfoResponseDto;
+import com.ajoufinder.api.controller.board.dto.response.BoardTempDto;
+import com.ajoufinder.api.service.location.LocationQueryService;
 import com.ajoufinder.api.service.user.UserQueryService;
 import com.ajoufinder.common.exception.ExceptionCode;
 import com.ajoufinder.common.exception.board.BoardException;
 import com.ajoufinder.domain.board.entity.Board;
+import com.ajoufinder.domain.board.entity.constant.BoardCategory;
+import com.ajoufinder.domain.board.entity.constant.BoardStatus;
 import com.ajoufinder.domain.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -20,6 +28,7 @@ import java.util.List;
 public class BoardQueryService {
   private final BoardRepository boardRepository;
   private final UserQueryService userQueryService;
+  private final LocationQueryService locationQueryService;
 
   public Board getBoardByIdOrThrow(Long boardId) {
     return boardRepository.findById(boardId).orElseThrow(()->new BoardException(ExceptionCode.NOT_FOUND_BOARD));
@@ -34,15 +43,50 @@ public class BoardQueryService {
   }
 
   public Page<BoardSimpleInfoResponseDto> getLostBoards(Pageable pageable) {
-    return boardRepository.getAllLostBoards(pageable);
+    Page<BoardTempDto>boardTempDtos= boardRepository.getAllLostBoards(pageable);
+    return boardTempDtos.map(BoardSimpleInfoResponseDto::from);
   }
 
   public Page<BoardSimpleInfoResponseDto> getFoundBoards(Pageable pageable) {
-    return boardRepository.getAllFoundBoards(pageable);
+    Page<BoardTempDto>boardTempDtos= boardRepository.getAllFoundBoards(pageable);
+    return boardTempDtos.map(BoardSimpleInfoResponseDto::from);
   }
 
   public List<BoardSimpleInfoResponseDto> getBoardsByUserId(Long userId) {
     userQueryService.getUserByIdOrThrow(userId);
-    return boardRepository.getBoardsByUserId(userId);
+    List<BoardTempDto>boardTempDtos= boardRepository.getBoardsByUserId(userId);
+    return boardTempDtos.stream().map(BoardSimpleInfoResponseDto::from)
+            .collect(Collectors.toList());
+  }
+
+  public Page<BoardSimpleInfoResponseDto> getBoardsByFilter(
+          LocalDate date,
+          LocalTime time,
+          Long locationId,
+          BoardStatus boardStatus,
+          BoardCategory boardCategory,
+          Pageable pageable
+  ) {
+    LocalDateTime start = null;
+    LocalDateTime end = null;
+
+    if (locationId != null) {
+      locationQueryService.findLocationByIdOrThrow(locationId);
+    }
+
+    if (date != null) {
+      if (time != null) {
+        start = date.atTime(time);
+        end = start.plusHours(1);
+      }else {
+        start = date.atStartOfDay();
+        end = date.plusDays(1).atStartOfDay();
+      }
+    } else if (time != null) {
+      throw new BoardException(ExceptionCode.INVALID_TIME_WITHOUT_DATE);
+    }
+
+    Page<BoardTempDto> boardTempDtos= boardRepository.getBoardsByFilter(start, end, locationId, boardStatus, boardCategory, pageable);
+    return boardTempDtos.map(BoardSimpleInfoResponseDto::from);
   }
 }
