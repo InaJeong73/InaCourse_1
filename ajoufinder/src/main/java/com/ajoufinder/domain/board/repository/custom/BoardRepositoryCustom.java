c74178315a44a71ec41e2ab823bbaed871c6681a
JeongIna <jeongina89@gmail.com>
2024-11-12 16:02:02 +0900

    feat: 분실 시간/분실 위치/게시글 상태 조건에 맞게 필터링하여 조회하는 커스텀 함수 구현

--- ajoufinder/src/main/java/com/ajoufinder/domain/board/repository/custom/BoardRepositoryCustom.java
+++ ajoufinder/src/main/java/com/ajoufinder/domain/board/repository/custom/BoardRepositoryCustom.java
@@ -1,19 +1,24 @@
 package com.ajoufinder.domain.board.repository.custom;
 
 import com.ajoufinder.api.controller.board.dto.response.BoardDetailInfoResponseDto;
-import com.ajoufinder.api.controller.board.dto.response.BoardSimpleInfoResponseDto;
+import com.ajoufinder.api.controller.board.dto.response.BoardTempDto;
+import com.ajoufinder.domain.board.entity.constant.BoardCategory;
+import com.ajoufinder.domain.board.entity.constant.BoardStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 public interface BoardRepositoryCustom {
   BoardDetailInfoResponseDto findBoardWithUserAndLocation(Long boardId);
 
-  Page<BoardSimpleInfoResponseDto> getAllLostBoards(Pageable pageable);
+  Page<BoardTempDto> getAllLostBoards(Pageable pageable);
 
-  Page<BoardSimpleInfoResponseDto> getAllFoundBoards(Pageable pageable);
+  Page<BoardTempDto> getAllFoundBoards(Pageable pageable);
 
-  List<BoardSimpleInfoResponseDto> getBoardsByUserId(Long userId);
+  List<BoardTempDto> getBoardsByUserId(Long userId);
+
+  Page<BoardTempDto> getBoardsByFilter(LocalDateTime start, LocalDateTime end, Long locationId, BoardStatus boardStatus, BoardCategory boardCategory,Pageable pageable);
 }
